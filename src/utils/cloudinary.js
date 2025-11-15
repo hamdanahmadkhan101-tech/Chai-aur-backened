ef50425223ab79bda473b833854aeca8ce05af58
hamdanahmadkhan101-tech <hamdanahmadkhan101@gmail.com>
2025-11-15 18:43:42 +0500

    added cloudinary utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "my_cloud",
+  api_key: process.env.CLOUDINARY_API_KEY || "my_key",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "my_secret",
+});
+
+export const uploadToCloudinary = async (filePath) => {
+  try {
+    if (!fs.existsSync(filePath)) {
+      throw new Error("File does not exist at the specified path");
+    }
+
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+    console.log("Upload to Cloudinary successful:", result.url);
+    // Remove the local file after upload
+    if (fs.existsSync(filePath)) {
+      await fs.promises.unlink(filePath);
+      console.log("Local file deleted:", filePath);
+    }
+    return result;
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    throw error;
+  }
+};
